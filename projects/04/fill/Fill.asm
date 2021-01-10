b042e43cad55846d30d29eed2bba84cb8d16de55
David Souther <davidsouther+github@gmail.com>
2021-01-09 16:00:58 -0800

    Chapter 4 - fill and multiply programs

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -11,4 +11,59 @@
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+@R0 // pattern // 0 for clearing, -1 for setting
+M=0
+
+@R2 // pixel // 256 rows
+M=0
+
+(LOOP)
+    // Clearing or setting?
+    @KBD
+    D=M
+    @SET
+    D;JGT
+
+    (CLEAR)
+    @R0 // pattern 
+    M=0
+    @CLEARSETEND
+    0;JMP
+
+    (SET)
+    @R0 // pattern 
+    M=-1
+
+    (CLEARSETEND)
+
+    // screen[pixel]=pattern
+    @R2
+    D=M
+    @SCREEN
+    D=D+A
+    @R1
+    M=D
+    // Memory to memory write
+    @R0
+    D=M // Load value to write into D
+    @R1 // Load address to write into A
+    A=M
+    M=D // Write into A (as M)
+
+    @R2
+    M=M+1
+
+    // if pixel >= 512 * 32 then pixel = 0
+    @R2 // pixel 
+    D=M
+    @8193
+    D=D-A
+    @NOSET
+    D;JLT
+    @R2 // pixel 
+    M=0
+    (NOSET)
+
+// Repeat this program forever
+@LOOP
+0;JMP
--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -6,4 +6,34 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Put your code here.
+
+// while(R0 > 0) {
+//     R2 = R2 + R1
+//     R0 = R0 - 1
+// }
+
+@R2
+M=0
+(LOOP)
+    // R0 > 0
+    @R0
+    D=M
+    @END
+    D;JEQ
+
+    // R2 = R1 + R2
+    @R1
+    D=M
+    @R2
+    D=D+M
+    M=D 
+
+    // R0 = R0 - 1
+    @R0
+    M=M-1
+    @LOOP
+    0;JMP
+(END)
+    @END
+    0;JMP
+

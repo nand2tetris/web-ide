6bc31decd0a8e8e7b7b741667fe65e7dff3c363b
David Souther <davidsouther+github@gmail.com>
2021-01-19 15:16:33 -0800

    Update package structures for nand2tetris

--- nand2tetris/build.sh
+++ nand2tetris/build.sh
@@ -2,29 +2,11 @@
 
 shopt -s globstar
 
-declare -A PACKAGES
-PACKAGES[HackPackageSource]=Hack
-PACKAGES[HackGUIPackageSource]=HackGUI
-PACKAGES[CompilersPackageSource]=Compilers
-PACKAGES[SimulatorsPackageSource]=Simulators
-PACKAGES[SimulatorsGUIPackageSource]=SimulatorsGUI
+mkdir --parents InstallDir/build
+rm -rf InstallDir/build/*
 
-#PACKAGES[BuiltInChipsSource] = BuiltInChips
-#PACKAGES[BuiltInVMCodeSource] = BuiltInVM
-#PACKAGES[MainClassesSource
-
-CLASSPATH="."
-for pkg in ${!PACKAGES[@]}; do
-    build="${pkg}/build"
-    mkdir --parents $build
-    CLASSPATH="${build}:${CLASSPATH}"
-done
-
-export CLASSPATH
-echo $CLASSPATH
-
-for pkg in ${!PACKAGES[@]}; do
-    jar=${PACKAGES[$pkg]}.jar
-    javac -d $pkg/build $pkg/**/*.java
-    jar cvf InstallDir/bin/lib/$jar $pkg/build/**/*.class
-done
+javac -d InstallDir/build org/**/*.java
+cd InstallDir
+jar cvf nand2tetris.jar bin
+cd build
+jar uvf ../nand2tetris.jar org

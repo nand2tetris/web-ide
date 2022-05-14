0da5aff8c2cea72852d6a41e2e6e5405f35ea5bc
David Souther <davidsouther+github@gmail.com>
2022-05-13 18:38:17 -0700

    Runbar uses input-group

--- src/components/runbar.ts
+++ src/components/runbar.ts
@@ -1,7 +1,15 @@
 import { Select } from "@davidsouther/jiffies/components/select.js";
 import { DenormChildren } from "@davidsouther/jiffies/dom/dom.js";
 import { FC } from "@davidsouther/jiffies/dom/fc.js";
-import { button, i, li, nav, ul } from "@davidsouther/jiffies/dom/html.js";
+import {
+  a,
+  button,
+  div,
+  i,
+  li,
+  nav,
+  ul,
+} from "@davidsouther/jiffies/dom/html.js";
 import { Timer } from "../simulator/timer.js";
 
 const icon = (icon: string) => i({ class: `icon-${icon}` });
@@ -9,77 +17,81 @@ const icon = (icon: string) => i({ class: `icon-${icon}` });
 export const Runbar = FC(
   "run-bar",
   (el, { runner }: { runner: Timer }, children: DenormChildren[]) =>
-    nav(
-      ul(
-        li(
-          button(
-            {
-              events: { click: () => runner.frame() },
+    div(
+      { class: "input-group" },
+      a(
+        {
+          href: "#",
+          role: "button",
+          events: {
+            click: (e) => {
+              e.preventDefault();
+              runner.frame();
             },
-            icon("fast-fw")
-          )
-        ),
-        li(
-          button(
-            {
-              events: { click: () => runner.reset() },
-            },
-            icon(`to-start`)
-          )
-        ),
-        li(
-          button(
-            {
-              events: {
-                click: () => (runner.running ? runner.stop() : runner.start()),
-              },
-            },
-            runner.running ? icon(`pause`) : icon(`play`)
-          )
-        )
+          },
+        },
+        icon("fast-fw")
       ),
-      ul(
-        li(
-          Select({
-            name: "speed",
-            events: {
-              change: (e: Event) => {
-                runner.speed = Number(
-                  (e.target as HTMLSelectElement)?.value ?? runner.speed
-                );
-              },
+      a(
+        {
+          role: "button",
+          events: {
+            click: (e) => {
+              e.preventDefault();
+              runner.reset();
             },
-            disabled: runner.running,
-            value: `${runner.speed}`,
-            options: [
-              ["16", "60FPS"],
-              ["500", "Fast"],
-              ["1000", "Normal"],
-              ["2000", "Slow"],
-            ],
-          })
-        ),
-        li(
-          Select({
-            name: "steps",
-            events: {
-              change: (e: Event) => {
-                runner.steps = Number(
-                  (e.target as HTMLSelectElement)?.value ?? runner.steps
-                );
-              },
-            },
-            disabled: runner.running,
-            value: `${runner.steps}`,
-            options: [
-              ["1", "1 Step"],
-              ["500", "500"],
-              ["1000", "1000"],
-              ["2000", "2000"],
-            ],
-          })
-        )
+          },
+        },
+        icon(`to-start`)
       ),
+      a(
+        {
+          role: "button",
+          events: {
+            click: (e) => {
+              e.preventDefault();
+              runner.running ? runner.stop() : runner.start();
+            },
+          },
+        },
+        runner.running ? icon(`pause`) : icon(`play`)
+      ),
+      Select({
+        name: "speed",
+        events: {
+          change: (e: Event) => {
+            runner.speed = Number(
+              (e.target as HTMLSelectElement)?.value ?? runner.speed
+            );
+          },
+        },
+        disabled: runner.running,
+        value: `${runner.speed}`,
+        options: [
+          ["16", "60FPS"],
+          ["500", "Fast"],
+          ["1000", "Normal"],
+          ["2000", "Slow"],
+        ],
+      }),
+      Select({
+        name: "steps",
+        events: {
+          change: (e: Event) => {
+            runner.steps = Number(
+              (e.target as HTMLSelectElement)?.value ?? runner.steps
+            );
+          },
+        },
+        disabled: runner.running,
+        value: `${runner.steps}`,
+        options: [
+          ["1", "1 Step"],
+          ["500", "500"],
+          ["1000", "1000"],
+          ["2000", "2000"],
+        ],
+      }),
       ...children
     )
 );

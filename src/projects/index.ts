5e4b6bdee6f1161a4ce22d4c5472761299736b39
David Souther <davidsouther+github@gmail.com>
2022-06-18 15:35:49 -0700

    Project 5 files
    Closes #9

--- src/projects/index.ts
+++ src/projects/index.ts
@@ -3,15 +3,18 @@ import { FileSystem } from "@davidsouther/jiffies/fs.js";
 import * as project_01 from "./project_01/index.js";
 import * as project_02 from "./project_02/index.js";
 import * as project_03 from "./project_03/index.js";
+import * as project_05 from "./project_05/index.js";
 
 export async function resetFiles(fs: FileSystem) {
   await project_01.resetFiles(fs);
   await project_02.resetFiles(fs);
   await project_03.resetFiles(fs);
+  await project_05.resetFiles(fs);
 }
 
 export async function loadSolutions(fs: FileSystem) {
   await project_01.loadSolutions(fs);
   await project_02.loadSolutions(fs);
   await project_03.loadSolutions(fs);
+  await project_05.loadSolutions(fs);
 }

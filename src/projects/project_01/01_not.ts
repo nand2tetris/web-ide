d3de90d864d97027128e9a4225678d50af88513a
David Souther <davidsouther+github@gmail.com>
2022-07-21 15:37:37 -0700

    Fix up test formatting.

--- src/projects/project_01/01_not.ts
+++ src/projects/project_01/01_not.ts
@@ -18,8 +18,7 @@ CHIP Not {
     PARTS:
     Nand(a=in, b=in, out=out);
 }`;
-export const tst = `
-output-list in%B3.1.3 out%B3.1.3;
+export const tst = `output-list in%B3.1.3 out%B3.1.3;
 
 set in 0, eval, output;
 set in 1, eval, output;`;

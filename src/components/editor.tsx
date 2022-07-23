f0198612b9dcb443afc9a12844931df18bb2150c
David Souther <davidsouther+github@gmail.com>
2022-07-22 17:53:17 -0700

    Use markers when using Monaco

--- src/components/editor.tsx
+++ src/components/editor.tsx
@@ -1,7 +1,7 @@
 import { debounce } from "@davidsouther/jiffies/lib/esm/debounce";
 import { Trans } from "@lingui/macro";
-import MonacoEditor from "@monaco-editor/react";
-import * as monaco from "monaco-editor/esm/vs/editor/editor.api";
+import MonacoEditor, { Monaco } from "@monaco-editor/react";
+import * as monacoT from "monaco-editor/esm/vs/editor/editor.api";
 import { OnMount } from "@monaco-editor/react";
 import ohm from "ohm-js";
 import {
@@ -18,14 +18,16 @@ import { UNKNOWN_PARSE_ERROR } from "../languages/base";
 
 import "./editor.scss";
 
-export const ErrorPanel = ({ error }: { error: string }) => {
-  return error.length ? (
+export const ErrorPanel = ({ error }: { error?: ohm.MatchResult }) => {
+  return error?.failed() ? (
     <details className="ErrorPanel" open>
       <summary role="button" className="secondary">
         <Trans>Parse Error</Trans>
       </summary>
       <pre>
-        <code>{error}</code>
+        <code>
+          {error?.message ?? error?.shortMessage ?? UNKNOWN_PARSE_ERROR}
+        </code>
       </pre>
     </details>
   ) : (
@@ -46,17 +48,13 @@ export const Editor = ({
   grammar: ohm.Grammar;
   language: string;
 }) => {
-  const [error, setError] = useState("");
+  const [error, setError] = useState<ohm.MatchResult>();
   const { theme } = useContext(AppContext);
 
   const parse = useCallback(
     (text: string = "") => {
       const parsed = grammar.match(text);
-      setError(
-        parsed.failed()
-          ? parsed.message ?? parsed.shortMessage ?? UNKNOWN_PARSE_ERROR
-          : ""
-      );
+      setError(parsed.failed() ? parsed : undefined);
     },
     [setError, grammar]
   );
@@ -72,7 +70,8 @@ export const Editor = ({
     [doParse, onChange]
   );
 
-  const editor = useRef<monaco.editor.IStandaloneCodeEditor>();
+  const editor = useRef<monacoT.editor.IStandaloneCodeEditor>();
+  const monaco = useRef<Monaco>();
 
   useEffect(() => {
     const isDark =
@@ -84,8 +83,47 @@ export const Editor = ({
     });
   }, [editor, theme]);
 
-  const onMount: OnMount = useCallback((ed) => {
+  useEffect(() => {
+    if (editor.current === undefined) return;
+    if (monaco.current === undefined) return;
+    const model = editor.current.getModel();
+    if (model === null) return;
+    if (error === undefined || error.succeeded()) {
+      monaco.current.editor.setModelMarkers(model, language, []);
+      return;
+    }
+    // Line 7, col 5:
+    const { line, column, message } =
+      /Line (?<line>\d+), col (?<column>\d+): (?<message>.*)/.exec(
+        error.shortMessage ?? ""
+      )?.groups ?? { line: 1, column: 1, message: "could not parse error" };
+    const startLineNumber = Number(line);
+    const endLineNumber = startLineNumber;
+    const startColumn = Number(column);
+    const restOfLine = model
+      .getLineContent(startLineNumber)
+      .substring(startColumn - 1);
+    let endColumn =
+      startColumn + (restOfLine.match(/([^\s]+)/)?.[0].length ?? 1);
+    if (endColumn <= startColumn) {
+      endColumn = startColumn + 1;
+    }
+
+    monaco.current.editor.setModelMarkers(model, language, [
+      {
+        message,
+        startColumn,
+        startLineNumber,
+        endColumn,
+        endLineNumber,
+        severity: monacoT.MarkerSeverity.Error,
+      },
+    ]);
+  }, [error, editor, monaco, language]);
+
+  const onMount: OnMount = useCallback((ed, mon) => {
     editor.current = ed;
+    monaco.current = mon;
     editor.current?.updateOptions({
       fontFamily: `"JetBrains Mono", source-code-pro, Menlo, Monaco,
       Consolas, "Roboto Mono", "Ubuntu Monospace", "Noto Mono", "Oxygen Mono",
@@ -109,7 +147,7 @@ export const Editor = ({
         language={language}
         onMount={onMount}
       />
-      <ErrorPanel error={error} />
+      {/* <ErrorPanel error={error} /> */}
     </div>
   );
 };

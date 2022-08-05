74f550a5d13ee9d4a8c8c3e741b6a566bdd481e6
David Souther <davidsouther+github@gmail.com>
2022-08-05 16:31:23 -0700

    Read and compile .asm files into memory

--- src/util/asm.test.ts
+++ src/util/asm.test.ts
@@ -1,4 +1,5 @@
-import { asm } from "./asm";
+import { ASSIGN, COMMANDS, JUMP } from "../simulator/cpu/alu";
+import { asm, makeC } from "./asm";
 
 describe("asm", () => {
   it("converts int16 to asm", () => {
@@ -10,5 +11,20 @@ describe("asm", () => {
     expect(asm(0b1110_001110_010_101)).toBe("D=D-1;JNE");
     expect(asm(0b1110_101010_000_111)).toBe("0;JMP");
     expect(asm(0b1111_110010_011_000)).toBe("MD=M-1");
+  });
+
+  it("makes C instruction", () => {
+    expect(makeC(true, COMMANDS.asm["D"], ASSIGN.asm["M"], JUMP.asm[""])).toBe(
+      0b111_1_001100_001_000
+    );
+    expect(
+      makeC(true, COMMANDS.asm["D-M"], ASSIGN.asm["D"], JUMP.asm[""])
+    ).toBe(0b111_1_010011_010_000);
+    expect(
+      makeC(false, COMMANDS.asm["D"], ASSIGN.asm[""], JUMP.asm["JGT"])
+    ).toBe(0b111_0_001100_000_001);
+    expect(
+      makeC(false, COMMANDS.asm["0"], ASSIGN.asm[""], JUMP.asm["JMP"])
+    ).toBe(0b111_0_101010_000_111);
   });
 });

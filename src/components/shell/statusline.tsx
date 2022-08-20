16d92010c81c330b0a610e38585c1b7826c10146
David Souther <davidsouther+github@gmail.com>
2022-08-20 15:09:32 -0700

    Load app-level sheets

--- src/components/shell/statusline.tsx
+++ src/components/shell/statusline.tsx
@@ -3,7 +3,7 @@ import { AppContext } from "../../App.context";
 
 function StatusLine() {
   const { status } = useContext(AppContext);
-  return <div>{status || "&nbsp;"}</div>;
+  return <div>{status}</div>;
 }
 
 export default StatusLine;

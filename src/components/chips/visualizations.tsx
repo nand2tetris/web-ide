9132c3222f5c58c4b7f3e9d334d3f06b408363a6
David Souther <davidsouther+github@gmail.com>
2022-08-20 12:18:22 -0700

    Show "None" for empty vis panel

--- src/components/chips/visualizations.tsx
+++ src/components/chips/visualizations.tsx
@@ -3,12 +3,13 @@ import { ReactNode } from "react";
 import { Chip } from "../../simulator/chip/chip";
 
 export const Visualizations = ({ parts }: { parts: Set<Chip> }) => {
-  if (parts.size === 0) {
-    return <Trans>None</Trans>;
-  }
   const visualizations: [string, ReactNode][] = [...parts].flatMap((part) =>
     part.render().map((v, i) => [`${part.id}_${i}`, v] as [string, ReactNode])
   );
+
+  if (visualizations.length === 0) {
+    return <Trans>None</Trans>;
+  }
 
   return (
     <>

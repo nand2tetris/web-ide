50784134c8b126bc65bd777228fc8069a0e50512
David Souther <davidsouther+github@gmail.com>
2022-08-20 12:41:07 -0700

    Plurals pass

--- src/urls.tsx
+++ src/urls.tsx
@@ -1,3 +1,4 @@
+import { t } from "@lingui/macro";
 import { lazy } from "react";
 
 const Home = lazy(() => import("./pages/home"));
@@ -6,12 +7,12 @@ const Util = lazy(() => import("./pages/util"));
 const Guide = lazy(() => import("./pages/user_guide"));
 
 const URLs = [
-  { href: "/", link: "Home", icon: "home", target: <Home /> },
-  { href: "/chip", link: "Chip", icon: "memory", target: <Chip /> },
-  // { href: "/cpu", link: "CPU", icon: "developer_board", target: <CPU /> },
-  // { href: "/vm", link: "VM", icon: "computer", target: <VM /> },
-  { href: "/util", link: "Conv", icon: "function", target: <Util /> },
-  { href: "/guide", link: "Guide", icon: "menu_book", target: <Guide /> },
+  { href: "/", link: t`Home`, icon: "home", target: <Home /> },
+  { href: "/chip", link: t`Chip`, icon: "memory", target: <Chip /> },
+  // { href: "/cpu", link: `CPU`, icon: "developer_board", target: <CPU /> },
+  // { href: "/vm", link: `VM`, icon: "computer", target: <VM /> },
+  { href: "/util", link: t`Conv`, icon: "function", target: <Util /> },
+  { href: "/guide", link: t`Guide`, icon: "menu_book", target: <Guide /> },
 ];
 
 export default URLs;

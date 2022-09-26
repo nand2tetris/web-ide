4e2fc163aabf1a459e68b745ed4c318e13e57c94
David Souther <dpsouth@amazon.com>
2022-09-25 21:27:13 -0700

    Conv page can update asm partially

--- src/app/pages/util.tsx
+++ src/app/pages/util.tsx
@@ -1,40 +1,76 @@
-import { ChangeEventHandler, useCallback, useState } from "react";
+import { ChangeEvent, useCallback, useState } from "react";
 import { asm, op } from "../../util/asm";
 import { bin, dec, hex, int10, int16, int2, uns } from "../../util/twos";
 
 export const Util = () => {
   const [value, setValue] = useState(0);
+  const [asmValue, setAsmValue] = useState("@0");
 
   const binValue = useCallback(() => bin(value), [value]);
   const decValue = useCallback(() => dec(value), [value]);
   const unsValue = useCallback(() => uns(value), [value]);
   const hexValue = useCallback(() => hex(value), [value]);
-  const asmValue = useCallback(() => asm(value), [value]);
+  // const asmValue = useCallback(() => asm(value), [value]);
 
-  const set =
-    (parse: (v: string) => number): ChangeEventHandler<HTMLInputElement> =>
-    ({ target: { value } }) => {
-      setValue(parse(value));
-    };
+  const setBin = useCallback(
+    ({ target: { value } }: ChangeEvent<HTMLInputElement>) => {
+      const ivalue = int2(value);
+      setValue(ivalue);
+      setAsmValue(asm(ivalue));
+    },
+    [setValue, setAsmValue]
+  );
+  const setInt = useCallback(
+    ({ target: { value } }: ChangeEvent<HTMLInputElement>) => {
+      const ivalue = int10(value);
+      setValue(ivalue);
+      setAsmValue(asm(ivalue));
+    },
+    [setValue, setAsmValue]
+  );
+  const setUns = useCallback(
+    ({ target: { value } }: ChangeEvent<HTMLInputElement>) => {
+      const ivalue = int10(value);
+      setValue(ivalue);
+      setAsmValue(asm(ivalue));
+    },
+    [setValue, setAsmValue]
+  );
+  const setHex = useCallback(
+    ({ target: { value } }: ChangeEvent<HTMLInputElement>) => {
+      const ivalue = int16(value);
+      setValue(ivalue);
+      setAsmValue(asm(ivalue));
+    },
+    [setValue, setAsmValue]
+  );
+  const setAsm = useCallback(
+    ({ target: { value } }: ChangeEvent<HTMLInputElement>) => {
+      setAsmValue(value);
+      setValue(op(value));
+    },
+    [setValue, setAsmValue]
+  );
 
   return (
     <form>
       <div className="grid">
         <label>
-          Binary <input type="text" value={binValue()} onChange={set(int2)} />
+          Binary <input type="text" value={binValue()} onChange={setBin} />
         </label>
         <label>
-          Decimal <input type="text" value={decValue()} onChange={set(int10)} />
+          Decimal <input type="text" value={decValue()} onChange={setInt} />
         </label>
         <label>
-          Unsigned{" "}
-          <input type="text" value={unsValue()} onChange={set(int10)} />
+          Unsigned
+          <input type="text" value={unsValue()} onChange={setUns} />
         </label>
         <label>
-          Hex <input type="text" value={hexValue()} onChange={set(int16)} />
+          Hex <input type="text" value={hexValue()} onChange={setHex} />
         </label>
         <label>
-          HACK ASM <input type="text" value={asmValue()} onChange={set(op)} />
+          HACK ASM
+          <input type="text" value={asmValue} onChange={setAsm} />
         </label>
       </div>
     </form>

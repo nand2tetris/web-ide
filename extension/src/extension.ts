726b57d64ce3b4fce920b4ca3a6b34064e9e8c98
David Souther <davidsouther@gmail.com>
2022-10-09 15:57:24 -0700

    Extension has error diagnostics.

--- extension/src/extension.ts
+++ extension/src/extension.ts
@@ -1,15 +1,33 @@
+console.log("Loading extension.js");
+
 import * as vscode from "vscode";
 import { hardware } from "./commands/hardware";
+import { getDiagnostics } from "./languages/hdl.js";
 
+let diagnosticCollection: vscode.DiagnosticCollection;
 export function activate(context: vscode.ExtensionContext) {
+  console.log("Activating extension");
   const hardwareCommand = vscode.commands.registerCommand(
     "computron5k.hardware",
     async (fileUri) => {
       await hardware(fileUri);
     }
   );
-
   context.subscriptions.push(hardwareCommand);
+
+  diagnosticCollection = vscode.languages.createDiagnosticCollection();
+  vscode.workspace.onDidChangeTextDocument(async (event) => {
+    if (event.document.languageId === "hdl") {
+      diagnosticCollection.clear();
+      const allDiagnostics = await getDiagnostics(event.document);
+      for (const [file, diagnostics] of allDiagnostics) {
+        diagnosticCollection.set(file, diagnostics);
+      }
+    }
+  });
+  context.subscriptions.push(diagnosticCollection);
 }
 
-export function deactivate() {}
+export function deactivate() {
+  console.log("Deactivating extension");
+}

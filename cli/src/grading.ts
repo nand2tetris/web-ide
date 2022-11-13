b39eb596d91653cbc68c49ccee04a935ccef8876
David Souther <davidsouther+github@gmail.com>
2022-11-13 12:00:44 -0800

    Added java shadow runner.

--- cli/src/grading.ts
+++ cli/src/grading.ts
@@ -1,53 +1,54 @@
-import { Assignments } from "@computron5k/projects/index.js";
+import { Assignment, Assignments } from "@computron5k/projects/index.js";
 import {
-  Assignment,
+  AssignmentFiles,
   hasTest,
   runTests,
 } from "@computron5k/simulator/projects/runner.js";
 import { FileSystem } from "@davidsouther/jiffies/lib/esm/fs.js";
+import { join, parse } from "path";
 
-import {
-  NodeFileSystemAdapter,
-  splitFile,
-} from "./node_file_system_adapter.js";
+import { NodeFileSystemAdapter } from "./node_file_system_adapter.js";
 
-async function loadAssignment(file: {
-  name: string;
-  hdl: string;
-}): Promise<Assignment> {
-  const assignment = Assignments[file.name as keyof typeof Assignments];
-  const tst = assignment[
-    `${file.name}.tst` as keyof typeof assignment
-  ] as string;
-  const cmp = assignment[
-    `${file.name}.cmp` as keyof typeof assignment
-  ] as string;
-  return { ...file, tst, cmp };
-}
+const loadAssignment = (fs: FileSystem) =>
+  async function (file: Assignment): Promise<AssignmentFiles> {
+    const assignment = Assignments[file.name as keyof typeof Assignments];
+    const hdl = await fs.readFile(file.base);
+    const tst = assignment[
+      `${file.name}.tst` as keyof typeof assignment
+    ] as string;
+    const cmp = assignment[
+      `${file.name}.cmp` as keyof typeof assignment
+    ] as string;
+    return { ...file, hdl, tst, cmp };
+  };
 
-export async function main(root = process.cwd()) {
+export async function main(folder = process.cwd(), java_ide = "") {
   const fs = new FileSystem(new NodeFileSystemAdapter());
-  fs.cd(root);
+  fs.cd(folder);
 
   const directory = [...(await fs.readdir("."))];
-  const runFiles = directory.filter(
-    (file) => file.endsWith(".hdl") || file.endsWith(".tst")
-  );
+  const runFiles = directory.filter((file) => file.endsWith(".hdl"));
 
-  console.log("Found files", runFiles);
+  const files = runFiles
+    .map((f) => join(folder, f))
+    .map(parse)
+    .filter(hasTest);
 
-  const files = await Promise.all(
-    runFiles
-      .map((file) => ({ file, ...splitFile(file) }))
-      .filter(hasTest)
-      .map(async (file) => {
-        const hdl = await fs.readFile(file.file);
-        return { ...file, hdl };
-      })
-  );
-
-  const tests = await runTests(files, loadAssignment, fs);
+  const tests = await runTests(files, loadAssignment(fs), fs, java_ide);
   for (const test of tests) {
-    console.log(`Test ${test.name}: ${test.pass ? "Passed" : "Failed"}`);
+    console.log(
+      `Test ${test.name}: ${test.pass ? `Passed` : `Failed (${test.out})`}`
+    );
+    if (test.shadow) {
+      console.log(
+        `\tShadow: ${
+          test.shadow.code === 0
+            ? `Passed`
+            : `Errored (${test.shadow.stderr.trim()})`
+        }`
+      );
+    } else {
+      console.log("\tNo shadow");
+    }
   }
 }

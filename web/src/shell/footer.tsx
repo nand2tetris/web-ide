44c3e931b74ea9d2afe5b597f2b9016f7f79614e
David Souther <davidsouther+github@gmail.com>
2023-10-29 20:17:17 -0400

    Added bug report link to footer (#197)

--- web/src/shell/footer.tsx
+++ web/src/shell/footer.tsx
@@ -9,6 +9,13 @@ const Footer = () => {
     <footer className="flex row justify-between">
       <StatusLine />
       <div className="flex row align-center">
+        <a
+          href="https://github.com/nand2tetris/web-ide/issues/new/choose"
+          target="new"
+        >
+          Report&nbsp;Bug
+        </a>
+        &nbsp;
         <button onClick={settings.open}>
           <Icon name="settings"></Icon>
         </button>

cacd3c042460ab89b0ce9dfd030bfd688b413fa4
David Souther <davidsouther+github@gmail.com>
2023-11-12 15:34:11 -0500

    Extension piece got missed

--- extension/src/commands/hardware.ts
+++ extension/src/commands/hardware.ts
@@ -1,6 +1,6 @@
 export async function hardware(fileUri: string) {
   // The await eval() hack is for https://github.com/microsoft/TypeScript/issues/43329
-  const tst = await import("@nand2tetris/simulator/tst.js");
+  const tst = await import("@nand2tetris/simulator/test/chiptst.js");
   console.log(`Hardware for ${fileUri}`);
   console.log(new tst.ChipTest());
 }

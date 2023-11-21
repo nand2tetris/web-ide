a2e5ab4f1ef004f28efd4ccb84bcb9f60cffb80a
Neta London <neta.london@gmail.com>
2023-11-21 15:35:43 +0200

    Replace speed control dropdowns with a single slider

--- components/src/runbar.tsx
+++ components/src/runbar.tsx
@@ -1,9 +1,33 @@
-import { ReactNode } from "react";
 import { Timer } from "@nand2tetris/simulator/timer.js";
+import { ChangeEvent, ReactNode, useEffect, useState } from "react";
 import { useTimer } from "./timer.js";
 
 export const Runbar = (props: { runner: Timer; children?: ReactNode }) => {
   const runner = useTimer(props.runner);
+  const [speedValue, setSpeed] = useState(0);
+
+  const speedValues: Record<number, [number, number]> = {
+    0: [1000, 1],
+    1: [500, 1],
+    2: [16, 1],
+    3: [16, 16666],
+    4: [16, 16666 * 30],
+  };
+
+  useEffect(() => {
+    updateSpeed();
+  }, [speedValue]);
+
+  const updateSpeed = () => {
+    const [speed, steps] = speedValues[speedValue];
+    runner.dispatch({ action: "setSpeed", payload: speed });
+    runner.dispatch({ action: "setSteps", payload: steps });
+  };
+
+  const onChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setSpeed(Number(e.target.value));
+  };
+
   return (
     <fieldset role="group">
       <button
@@ -34,62 +58,17 @@ export const Runbar = (props: { runner: Timer; children?: ReactNode }) => {
       >
         {/* <Icon name="fast_rewind" /> */}⏮
       </button>
-      <select
-        className="flex-1"
-        name="speed"
-        value={runner.state.speed}
-        onChange={(e) => {
-          runner.dispatch({
-            action: "setSpeed",
-            payload: Number(e.target?.value ?? runner.state.speed),
-          });
-        }}
+      <span>Slow</span>
+      <input
+        type="range"
+        min={0}
+        max={4}
+        step={1}
+        value={speedValue}
         disabled={runner.state.running}
-      >
-        {[
-          // [2000, `Debug`], // For debugging, basically the same as clicking through
-          [1000, `Slow`],
-          [500, `Fast`],
-          [16, `Faster`], // 16ms, or 60fps
-        ].map(([speed, label]) => (
-          <option key={speed} value={speed}>
-            {label}
-          </option>
-        ))}
-      </select>
-      <select
-        className="flex-1"
-        name="steps"
-        value={runner.state.steps}
-        onChange={(e) => {
-          runner.dispatch({
-            action: "setSteps",
-            payload: Number(e.target?.value ?? runner.state.steps),
-          });
-        }}
-        disabled={runner.state.running}
-      >
-        {[
-          ...[
-            [1, `1 Step`],
-            [500, "500"],
-            [1000, "1000"],
-            [2000, "2000"],
-            [8000, "8000"],
-          ],
-          ...(runner.state.speed === 16
-            ? [
-                // 60fps
-                [16666, "1MHz"],
-                [16666 * 30, "30MHz"],
-              ]
-            : []),
-        ].map(([steps, label]) => (
-          <option key={steps} value={steps}>
-            {label}
-          </option>
-        ))}
-      </select>
+        onChange={onChange}
+      ></input>
+      <span>Fast</span>
       {props.children}
     </fieldset>
   );

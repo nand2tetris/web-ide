8dce385a613e4d19f1d546baa3b90610dd000808
Neta London <neta.london@gmail.com>
2023-11-23 10:01:49 +0200

    Add placeholder page for assembler

---
+++ web/src/pages/asm.tsx
@@ -0,0 +1,5 @@
+export const CPU = () => {
+  return <div>Assembler</div>;
+};
+
+export default CPU;
--- web/src/urls.tsx
+++ web/src/urls.tsx
@@ -4,6 +4,7 @@ import { lazy } from "react";
 const Home = lazy(() => import("./pages/home"));
 const Chip = lazy(() => import("./pages/chip"));
 const CPU = lazy(() => import("./pages/cpu"));
+const ASM = lazy(() => import("./pages/asm"));
 const Util = lazy(() => import("./pages/util"));
 const Guide = lazy(() => import("./pages/user_guide"));
 
@@ -11,6 +12,7 @@ const URLs = [
   { href: "/", link: t`Home`, icon: "home", target: <Home /> },
   { href: "/chip", link: t`Chip`, icon: "memory", target: <Chip /> },
   { href: "/cpu", link: `CPU`, icon: "developer_board", target: <CPU /> },
+  { href: "/asm", link: t`Asm`, icon: "list_alt", target: <ASM /> },
   // { href: "/vm", link: `VM`, icon: "computer", target: <VM /> },
   { href: "/util", link: t`Conv`, icon: "function", target: <Util /> },
   { href: "/guide", link: t`Guide`, icon: "menu_book", target: <Guide /> },

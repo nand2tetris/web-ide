d913186a01d3488d301619b757f955e618367edc
Neta London <neta.london@gmail.com>
2023-12-12 14:30:44 +0200

    Add about page content

--- web/src/pages/about.tsx
+++ web/src/pages/about.tsx
@@ -1,5 +1,14 @@
+import raw from "raw.macro";
+import Markdown from "../shell/markdown";
+
 export const About = () => {
-  return <span>To be added later</span>;
+  return (
+    <div style={{ overflowY: "scroll" }}>
+      <div style={{ maxWidth: "900px", margin: "0 auto" }}>
+        <Markdown>{raw("./ABOUT.md")}</Markdown>
+      </div>
+    </div>
+  );
 };
 
 export default About;

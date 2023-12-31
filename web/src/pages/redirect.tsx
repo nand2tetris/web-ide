f797aec97e4c3f71ad3bfc5e79a6e42f704d6e35
Neta London <neta.london@gmail.com>
2023-12-31 20:40:55 +0200

    Document redirect component

--- web/src/pages/redirect.tsx
+++ web/src/pages/redirect.tsx
@@ -3,6 +3,8 @@ import { Navigate } from "react-router-dom";
 import { AppContext } from "../App.context";
 import urls, { LAST_ROUTE_KEY } from "../urls";
 
+// Redirects the user to the last route they visited,
+// and handles any context changes that should happen before entering the new route
 export const Redirect = () => {
   const { toolStates } = useContext(AppContext);
   const [route, setRoute] = useState<string>();

9db8b32ebe923480cf463d1d59438968b4c65788
Neta London <neta.london@gmail.com>
2024-03-18 19:24:12 +0200

    Update ALU visualization

--- components/src/chips/alu.tsx
+++ components/src/chips/alu.tsx
@@ -17,13 +17,27 @@ export const ALUComponent = ({
   out: number;
   flag: keyof typeof Flags;
 }) => (
-  <div>
+  <div style={{ fontSize: 20 }}>
     <span>ALU</span>
     <svg width="250" height="250" xmlns="http://www.w3.org/2000/svg">
       <g>
-        <rect x="1" y="20" height="85" width="70" fill="black" />
-        <rect x="1" y="145" height="85" width="70" fill="black" />
-        <rect x="180" y="95" height="60" width="70" fill="black" />
+        <rect x="1" y="20" height="85" width="70" fill="white" stroke="black" />
+        <rect
+          x="1"
+          y="145"
+          height="85"
+          width="70"
+          fill="white"
+          stroke="black"
+        />
+        <rect
+          x="180"
+          y="95"
+          height="60"
+          width="70"
+          fill="white"
+          stroke="black"
+        />
         <polygon
           points="70,10 180,85 180,165 70,240 70,135 90,125 70,115"
           stroke="#000"
@@ -33,13 +47,10 @@ export const ALUComponent = ({
           xmlSpace="preserve"
           textAnchor="middle"
           fontFamily="Noto Sans JP"
-          fontSize="14"
-          strokeWidth="0"
           id="svg_9"
           y="63"
           x="35"
-          stroke="white"
-          fill="#ffffff"
+          fill="black"
         >
           {A}
         </text>
@@ -47,13 +58,10 @@ export const ALUComponent = ({
           xmlSpace="preserve"
           textAnchor="middle"
           fontFamily="Noto Sans JP"
-          fontSize="14"
           id="svg_10"
           y="188"
           x="35"
-          strokeWidth="0"
-          stroke="white"
-          fill="#ffffff"
+          fill="black"
         >
           {D}
         </text>
@@ -61,13 +69,10 @@ export const ALUComponent = ({
           xmlSpace="preserve"
           textAnchor="middle"
           fontFamily="Noto Sans JP"
-          fontSize="14"
           id="svg_11"
           y="125"
           x="215"
-          strokeWidth="0"
-          stroke="white"
-          fill="#ffffff"
+          fill="black"
         >
           {out}
         </text>
@@ -75,13 +80,10 @@ export const ALUComponent = ({
           xmlSpace="preserve"
           textAnchor="middle"
           fontFamily="Noto Sans JP"
-          fontSize="14"
           id="svg_13"
           y="125"
           x="135"
-          strokeWidth="0"
-          stroke="white"
-          fill="#ffffff"
+          fill="white"
         >
           {COMMANDS_ALU.op[op] ?? "(??)"}
         </text>

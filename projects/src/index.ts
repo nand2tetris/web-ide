869bba6de2be99c3fed675c2727c431c81d6c7f0
Neta London <neta.london@gmail.com>
2024-03-21 16:15:42 +0200

    add project 6 asm files

--- projects/src/index.ts
+++ projects/src/index.ts
@@ -7,6 +7,7 @@ import * as project_04 from "./project_04/index.js";
 import * as project_05 from "./project_05/index.js";
 import * as project_07 from "./project_07/index.js";
 import * as project_08 from "./project_08/index.js";
+import * as project_06 from "./samples/project_06/index.js";
 
 /**
  * Duplicated for web from node:path.
@@ -45,6 +46,7 @@ const Projects = {
   3: project_03,
   4: project_04,
   5: project_05,
+  6: project_06,
   7: project_07,
   8: project_08,
 };
@@ -53,7 +55,7 @@ let reset = false;
 export const resetFiles = async (fs: FileSystem, projects?: number[]) => {
   if (reset) return; // React will double-render a call to resetFiles in useEffect.
   reset = true;
-  projects ??= [1, 2, 3, 4, 5, 7, 8];
+  projects ??= [1, 2, 3, 4, 6, 5, 7, 8];
   for (const project of projects) {
     if (!Object.keys(Projects).includes(project.toString())) {
       continue;
@@ -76,10 +78,7 @@ export const resetTests = async (fs: FileSystem, projects?: number[]) => {
   reset = false;
 };
 
-export const BUILTIN_CHIP_PROJECTS: Record<
-  "1" | "2" | "3" | "5",
-  string[]
-> = {
+export const BUILTIN_CHIP_PROJECTS: Record<"1" | "2" | "3" | "5", string[]> = {
   "1": ["Nand"],
   "2": [],
   "3": ["DFF"],
@@ -116,20 +115,19 @@ export const CHIP_PROJECTS: Record<"1" | "2" | "3" | "5", string[]> = {
   "5": ["Memory", "CPU", "Computer"],
 };
 
-export const CHIP_ORDER: Partial<Record<"1" | "2" | "3" | "5", string[]>> =
-  {
-    "5": [
-      "Memory",
-      "CPU",
-      "Computer",
-      "Screen",
-      "Keyboard",
-      "DRegister",
-      "ARegister",
-      "ROM32K",
-      "RAM16K",
-    ],
-  };
+export const CHIP_ORDER: Partial<Record<"1" | "2" | "3" | "5", string[]>> = {
+  "5": [
+    "Memory",
+    "CPU",
+    "Computer",
+    "Screen",
+    "Keyboard",
+    "DRegister",
+    "ARegister",
+    "ROM32K",
+    "RAM16K",
+  ],
+};
 
 export const ASM_PROJECTS: Record<"6", string[]> = {
   "6": ["Add", "Max", "Rectangle", "Pong"],

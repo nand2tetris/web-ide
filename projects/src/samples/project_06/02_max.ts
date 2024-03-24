f0d36b3409c4858dd6b2d838d7891c811a95fe48
Neta London <neta.london@gmail.com>
2024-03-24 16:14:04 +0200

    Update project 6 files

--- projects/src/samples/project_06/02_max.ts
+++ projects/src/samples/project_06/02_max.ts
@@ -6,27 +6,36 @@ repeat 14 {
    tick, tock;
 }
 `;
-export const MaxAsm = `
-// Computes R2 = max(R0, R1)
+export const MaxAsm = `// This file is part of www.nand2tetris.org
+// and the book "The Elements of Computing Systems"
+// by Nisan and Schocken, MIT Press.
+// File name: projects/6/max/Max.asm
 
+// Computes R2 = max(R0, R1)  (R0,R1,R2 refer to RAM[0],RAM[1],RAM[2])
+// Usage: Before executing, put two values in R0 and R1.
+
+   // D = R0 - R1
    @R0
-   D=M              // D = first number
+   D=M
    @R1
-   D=D-M            // D = first number - second number
-   @OUTPUT_FIRST
-   D;JGT            // if D>0 (first is greater) goto output_first
+   D=D-M
+   // If (D > 0) goto ITSR0
+   @ITSR0
+   D;JGT
+   // Its R1
    @R1
-   D=M              // D = second number
-   @OUTPUT_D
-   0;JMP            // goto output_d
-(OUTPUT_FIRST)
-   @R0             
-   D=M              // D = first number
-(OUTPUT_D)
+   D=M
    @R2
-   M=D              // M[2] = D (greatest number)
-(INFINITE_LOOP)
-   @INFINITE_LOOP
+   M=D
+   @END
+   0;JMP
+(ITSR0)
+   @R0             
+   D=M
+   @R2
+   M=D
+(END)
+   @END
    0;JMP
 `;
 

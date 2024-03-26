3be794f52a3850e663e95f5aa5a358014a57703f
netalondon <67196883+netalondon@users.noreply.github.com>
2024-03-26 22:25:04 +0200

    Update hardware simulator controls (#252)
    * Remove reset button from hardware simulator
    * Add tooltip to hardware simulator download button

--- web/src/pages/chip.tsx
+++ web/src/pages/chip.tsx
@@ -214,15 +214,10 @@ export const Chip = () => {
 
         <button
           className="flex-0"
-          onClick={actions.resetFile}
-          disabled={state.controls.builtinOnly}
-        >
-          <Trans>Reset</Trans>
-        </button>
-        <button
-          className="flex-0"
           onClick={downloadProject}
           disabled={state.controls.builtinOnly}
+          data-tooltip={t`Download .hdl files`}
+          data-placement="left"
         >
           <Trans>Download</Trans>
         </button>

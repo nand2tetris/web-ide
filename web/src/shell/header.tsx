4ee4e78145f7f25e0ecb4dbb90059ee5d54f3307
Neta London <neta.london@gmail.com>
2024-05-07 20:11:53 +0300

    Add VM emulator user guide

--- web/src/shell/header.tsx
+++ web/src/shell/header.tsx
@@ -41,6 +41,7 @@ const guideLinks: Record<string, string> = {
   chip: "https://drive.google.com/file/d/15unXGgTfQySMr1V39xTCLTgGfCOr6iG9/view",
   cpu: "https://drive.google.com/file/d/16eHIj78Cpeb0uxXBAvxUPUaIwkrj3NIu/view",
   asm: "https://drive.google.com/file/d/16gy2EDqUqrPIzy-vyX0-M8ObN5HyQa3Y/view",
+  vm: "https://drive.google.com/file/d/1IMKD_khjO7iQ673kH9j8qMkyIlMSOtUG/view"
 };
 
 const GUIDE_NOT_AVAILABLE_MESSAGE = "Guide not available for this tool";

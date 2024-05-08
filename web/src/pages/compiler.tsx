f9cdded6d844cd10806b3f2d55122a75ebe9453b
Neta London <neta.london@gmail.com>
2024-05-08 09:15:27 +0300

    Remove editing features

--- web/src/pages/compiler.tsx
+++ web/src/pages/compiler.tsx
@@ -11,7 +11,6 @@ import {
   useContext,
   useEffect,
   useRef,
-  useState,
 } from "react";
 import { Link } from "react-router-dom";
 import { useDialog } from "src/shell/dialog";
@@ -28,10 +27,6 @@ export const Compiler = () => {
   const uploadRef = useRef<HTMLInputElement>(null);
   const downloadRef = useRef<HTMLAnchorElement>(null);
   const redirectRef = useRef<HTMLAnchorElement>(null);
-
-  const newFileDialog = useDialog();
-  const renameDialog = useDialog();
-  const deleteDialog = useDialog();
 
   useEffect(() => {
     actions.initialize();
@@ -101,77 +96,6 @@ export const Compiler = () => {
     redirectRef.current?.click();
   };
 
-  const isNameValid = (name: string) => {
-    return (
-      (name?.match(/^[a-zA-Z_][a-zA-Z0-9_]*$/) &&
-        !Object.keys(state.files).includes(name)) ??
-      false
-    );
-  };
-
-  const newFileDialogComponent = (
-    <NameDialog
-      title="Create New File"
-      buttonText={"Create"}
-      dialog={newFileDialog}
-      isValid={isNameValid}
-      onExit={(name?: string) => {
-        if (name) {
-          actions.addFile(name);
-        }
-      }}
-    />
-  );
-
-  const [current, setCurrent] = useState<string>();
-
-  const renameDialogComponent = (
-    <NameDialog
-      title={`Rename ${current}.jack`}
-      buttonText={"Rename"}
-      dialog={renameDialog}
-      isValid={isNameValid}
-      onExit={(name?: string) => {
-        if (name) {
-          if (!current) {
-            return;
-          }
-          actions.renameFile(current, name);
-        }
-      }}
-    />
-  );
-
-  const deleteDialogComponent = (
-    <dialog open={deleteDialog.isOpen}>
-      <article>
-        <header>
-          <p>Are you sure you want to delete {current}.jack?</p>
-        </header>
-        <main>
-          <button
-            onClick={() => {
-              deleteDialog.close();
-            }}
-          >
-            Cancel
-          </button>
-          <button
-            onClick={async () => {
-              if (!current) {
-                return;
-              }
-              await actions.deleteFile(current);
-              deleteDialog.close();
-            }}
-          >
-            Delete
-          </button>
-        </main>
-      </article>
-    </dialog>
-  );
-
   return (
     <div className="Page CompilerPage grid">
       <a style={{ display: "none" }} ref={downloadRef}></a>
@@ -187,41 +111,20 @@ export const Compiler = () => {
         to={URLs["vm"].href}
         style={{ display: "none" }}
       />
-      {newFileDialogComponent}
-      {renameDialogComponent}
-      {deleteDialogComponent}
-      <Panel
-        className="files"
-        header={
-          <>
-            <Trans>Files</Trans>
-            <fieldset role="group">
-              <button onClick={newFileDialog.open}>New</button>
-              <button onClick={uploadFiles}>📂</button>
-            </fieldset>
-          </>
-        }
-      >
-        {Object.keys(state.files).map((file) => (
-          <FileEntry
-            key={file}
-            name={file}
-            onRename={() => {
-              setCurrent(file);
-              renameDialog.open();
-            }}
-            onDelete={() => {
-              setCurrent(file);
-              deleteDialog.open();
-            }}
-          />
-        ))}
-      </Panel>
       <Panel
         className="code"
         header={
           <>
-            <Trans>Source</Trans>
+            <div>
+              <Trans>Source</Trans>
+            </div>
+            <div className="flex-1">
+              <fieldset role="group">
+                <button className="flex-0" onClick={uploadFiles}>
+                  📂
+                </button>
+              </fieldset>
+            </div>
             <fieldset role="group">
               <button
                 disabled={!valid()}
@@ -280,8 +183,9 @@ export const Compiler = () => {
               >
                 <Editor
                   value={state.files[file].content}
+                  disabled={true}
                   onChange={(source: string) => {
-                    actions.editFile(file, source);
+                    return;
                   }}
                   error={state.files[file].error}
                   language={""}
@@ -292,77 +196,6 @@ export const Compiler = () => {
         </div>
       </Panel>
     </div>
-  );
-};
-
-const FileEntry = ({
-  name,
-  onRename,
-  onDelete,
-}: {
-  name: string;
-  onRename: () => void;
-  onDelete: () => void;
-}) => {
-  return (
-    <div className="flex row file-entry">
-      <span className="flex-1">{name}.jack</span>
-      <a onClick={onRename}>✏️</a>
-      <a onClick={onDelete}>🗑️</a>
-    </div>
-  );
-};
-
-const NameDialog = ({
-  title,
-  buttonText,
-  dialog,
-  isValid,
-  onExit,
-}: {
-  title: string;
-  buttonText: string;
-  dialog: ReturnType<typeof useDialog>;
-  isValid: (value: string) => boolean;
-  onExit: (value?: string) => void;
-}) => {
-  const [value, setValue] = useState<string>();
-
-  return (
-    <dialog open={dialog.isOpen}>
-      <article>
-        <header>
-          <Trans>{title}</Trans>
-          <a
-            style={{ color: "rgba(0, 0, 0, 0)" }}
-            className="close"
-            href="#root"
-            onClick={(e) => {
-              e.preventDefault();
-              onExit();
-              dialog.close();
-            }}
-          >
-            close
-          </a>
-        </header>
-        <main>
-          <input
-            value={value}
-            onChange={(e) => setValue(e.target.value)}
-          ></input>
-          <button
-            disabled={!isValid(value ?? "")}
-            onClick={() => {
-              dialog.close();
-              onExit(value);
-            }}
-          >
-            {buttonText}
-          </button>
-        </main>
-      </article>
-    </dialog>
   );
 };
 

f68bc40d8ca087398e9363c037aa8fbc984ce23d
Neta London <neta.london@gmail.com>
2024-06-04 12:28:13 +0300

    Embed user guide pdfs

--- web/src/shell/header.tsx
+++ web/src/shell/header.tsx
@@ -39,12 +39,11 @@ function headerButtonFromURL(url: URL, icon: string, tooltip?: string) {
 
 // When updating these, also edit service-worker.ts
 const guideLinks: Record<string, string> = {
-  chip: "https://drive.google.com/file/d/15unXGgTfQySMr1V39xTCLTgGfCOr6iG9/view",
-  cpu: "https://drive.google.com/file/d/16eHIj78Cpeb0uxXBAvxUPUaIwkrj3NIu/view",
-  asm: "https://drive.google.com/file/d/16gy2EDqUqrPIzy-vyX0-M8ObN5HyQa3Y/view",
-  vm: "https://drive.google.com/file/d/1IMKD_khjO7iQ673kH9j8qMkyIlMSOtUG/view",
-  compiler:
-    "https://drive.google.com/file/d/1KIxNH148mZET5JGE8Y0fPYVYKesgCH8V/view",
+  chip: "user_guide/chip.pdf",
+  cpu: "user_guide/cpu.pdf",
+  asm: "user_guide/asm.pdf",
+  vm: "user_guide/vm.pdf",
+  compiler: "user_guide/compiler.pdf",
 };
 
 const GUIDE_NOT_AVAILABLE_MESSAGE = "Guide not available for this tool";

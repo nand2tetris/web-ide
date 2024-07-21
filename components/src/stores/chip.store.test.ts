3c6235cfdda7ca97bf22638cacb138af0c8a997a
Neta London <neta.london@gmail.com>
2024-07-21 14:23:12 +0300

    Fix tests and test scripts

--- components/src/stores/chip.store.test.ts
+++ components/src/stores/chip.store.test.ts
@@ -4,7 +4,6 @@ import {
 } from "@davidsouther/jiffies/lib/esm/fs.js";
 import { cleanState } from "@davidsouther/jiffies/lib/esm/scope/state.js";
 import * as not from "@nand2tetris/projects/project_01/01_not.js";
-import * as bit from "@nand2tetris/projects/project_03/01_bit.js";
 import { produce } from "immer";
 import { MutableRefObject } from "react";
 import { ImmPin } from "src/pinout.js";
@@ -47,61 +46,40 @@ function testChipStore(
 
 describe("ChipStore", () => {
   describe("initialization", () => {
-    it("starts on project 01 not", async () => {
-      const { state } = testChipStore({});
-
-      expect(state.controls.project).toBe("01");
-      expect(state.controls.chipName).toBe("Not");
-      expect(state.files.hdl).toBe("");
-      expect(state.files.tst).toBe("");
-      expect(state.files.cmp).toBe("");
-      expect(state.files.out).toBe("");
-    });
-
-    it("reloads initial chip not", async () => {
+    it("loads chip", async () => {
       const store = testChipStore({
         "projects/01/Not/Not.hdl": not.hdl,
         "projects/01/Not/Not.tst": not.tst,
         "projects/01/Not/Not.cmp": not.cmp,
       });
 
-      //   await store.actions.initialize();
+      await store.actions.initialize();
+      await store.actions.loadChip("projects/01/Not/Not.hdl");
 
       expect(store.state.controls.project).toBe("01");
       expect(store.state.controls.chipName).toBe("Not");
       expect(store.state.files.hdl).toBe(not.hdl);
       expect(store.state.files.tst).toBe(not.tst);
-      expect(store.state.files.cmp).toBe(not.cmp);
+      expect(store.state.files.cmp).toBe("");
       expect(store.state.files.out).toBe("");
-    });
-
-    it("loads saved state", () => {
-      const { state } = testChipStore(
-        {
-          "projects/01/Not/Not.hdl": not.hdl,
-          "projects/01/Not/Not.tst": not.tst,
-          "projects/01/Not/Not.cmp": not.cmp,
-          "projects/03/Bit/Bit.hdl": bit.hdl,
-          "projects/03/Bit/Bit.tst": bit.tst,
-          "projects/03/Bit/Bit.cmp": bit.cmp,
-        },
-        {
-          "/chip/project": "03",
-          "/chip/chip": "Bit",
-        },
-      );
-      expect(state.controls.project).toBe("03");
-      expect(state.controls.chipName).toBe("Bit");
     });
   });
 
   describe("behavior", () => {
-    const state = cleanState(() => ({ store: testChipStore() }), beforeEach);
+    const state = cleanState(async () => {
+      const store = testChipStore({
+        "projects/01/Not/Not.hdl": not.hdl,
+        "projects/01/Not/Not.tst": not.tst,
+        "projects/01/Not/Not.cmp": not.cmp,
+      });
+      await store.actions.initialize();
+      await store.actions.loadChip("projects/01/Not/Not.hdl");
+      return { store };
+    }, beforeEach);
 
     it.todo("loads projects and chips");
 
     it("toggles bits", async () => {
-      //   await state.store.actions.initialize();
       state.store.actions.toggle(state.store.state.sim.chip[0].in(), 0);
       expect(state.store.state.sim.chip[0].in().busVoltage).toBe(1);
       expect(state.store.dispatch.current).toHaveBeenCalledWith({
@@ -127,7 +105,8 @@ describe("ChipStore", () => {
         "projects/01/Not/Not.tst": not.tst,
         "projects/01/Not/Not.cmp": not.cmp,
       });
-      //   await store.actions.initialize();
+      await store.actions.initialize();
+      await store.actions.loadChip("projects/01/Not/Not.hdl");
       return { store };
     }, beforeEach);
 
@@ -145,7 +124,9 @@ describe("ChipStore", () => {
       expect(bits(state.store.state.sim.inPins)).toEqual([[0]]);
       expect(bits(state.store.state.sim.outPins)).toEqual([[1]]);
 
-      await state.store.actions.stepTest(); // Output List
+      await state.store.actions.stepTest(); // Output List and Compare To
+
+      expect(state.store.state.files.cmp).toEqual(not.cmp);
 
       await state.store.actions.stepTest(); // Set in 0
       expect(bits(state.store.state.sim.inPins)).toEqual([[0]]);
@@ -155,7 +136,7 @@ describe("ChipStore", () => {
       expect(bits(state.store.state.sim.inPins)).toEqual([[1]]);
       expect(bits(state.store.state.sim.outPins)).toEqual([[0]]);
 
-      await state.store.actions.stepTest(); // No change (afte end)
+      await state.store.actions.stepTest(); // No change (after end)
       expect(bits(state.store.state.sim.inPins)).toEqual([[1]]);
       expect(bits(state.store.state.sim.outPins)).toEqual([[0]]);
     });
--- simulator/src/chip/builtins/all.test.ts
+++ simulator/src/chip/builtins/all.test.ts
@@ -25,7 +25,7 @@ describe("All Projects", () => {
     )("Builtin %s", async (chipName) => {
       // eslint-disable-next-line @typescript-eslint/ban-ts-comment
       // @ts-ignore
-      const ChipProject = ChipProjects[project]?.CHIPS[chipName];
+      const ChipProject = ChipProjects[project]?.CHIPS;
       let hdlFile: string = ChipProject?.[`${chipName}.hdl`];
       const tstFile: string = ChipProject?.[`${chipName}.tst`];
       const cmpFile: string = ChipProject?.[`${chipName}.cmp`];

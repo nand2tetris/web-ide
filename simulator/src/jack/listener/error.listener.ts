faef67d7c44e156ff80292a69f57d82aef35efec
Roman Lukash <2893931+RomanLukash340@users.noreply.github.com>
2024-10-03 19:26:46 +0200

    Fix lexer error handling

--- simulator/src/jack/listener/error.listener.ts
+++ simulator/src/jack/listener/error.listener.ts
@@ -1,5 +1,8 @@
-import { ErrorListener, RecognitionException, Recognizer, Token } from "antlr4";
+import { ErrorListener, NoViableAltException, RecognitionException, Recognizer, Token } from "antlr4";
 import { JackCompilerError, LexerOrParserError } from "../error.js";
+interface LexerNoViableAltException {
+  startIndex: number;
+}
 export class CustomErrorListener extends ErrorListener<any> {
   public errors: JackCompilerError[] = [];
 
@@ -11,7 +14,21 @@ export class CustomErrorListener extends ErrorListener<any> {
     msg: string,
     e: RecognitionException | undefined,
   ) => {
-    const t = offendingSymbol as Token;
-    this.errors.push(new LexerOrParserError(line, t.start, t.stop + 1, msg));
+    if (offendingSymbol != null || e != null && e.offendingToken != null) {
+      const t = offendingSymbol ?? e!.offendingToken as Token;
+      this.errors.push(new LexerOrParserError(line, t.start, t.stop + 1, msg));
+    } else if (e instanceof NoViableAltException) {
+      this.errors.push(new LexerOrParserError(line, e.startToken.start, e.startToken.stop + 1, msg));
+      
+    }
+    //antlr doesn't provide a class for LexerNoViableAltException atm. Once https://github.com/antlr/antlr4/pull/4711 is release we can change it
+    else if (e != null && 'startIndex' in e) {
+      const err = e as LexerNoViableAltException;
+      this.errors.push(new LexerOrParserError(line, err.startIndex, err.startIndex + 1, msg));
+    } else {
+      console.error("Don't know how to handle this error");
+      throw new Error("Don't know how to handle this error");
+    }
   };
 }
+

1ce8880c3e7f3545c2485893f06dbbca6ae2564f
David Souther <davidsouther@gmail.com>
2024-12-02 21:02:03 -0500

    Add stamp step & version notification in release builds (#512)
    * Reset workspace versions to "private" 0.0.0
    * Show embedded version in about page.
    * Add stamp script for release.
    * 2024.49.1
    
    ---------
    
    Co-authored-by: David Souther <david.souther+github@gmail.com>

---
+++ stamp.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+set -e # Exit on errors
+# set -x # Shell debugging
+
+# CURRENT and NEXT have the format YYYY.WW.REV, where YYYY is the current year,
+# WW is the current week, and REV is the number of releases this week.
+# The next revision compares the two, in this way
+# - If NEXT is later than CURRENT in any fields, accept NEXT.
+# - Otherwise, return CURRENT, with one added to REV.
+#
+# THIS FUNCTION IS NOT TRANSITIVE! It must be called with
+# `compare_versions CURRENT NEXT`
+compare_versions() {
+    if [[ "$1" < "$2" ]] ; then
+        echo "$2"
+    else
+        IFS='.' read -r y1 w1 r1 <<< "$1"
+        r1=$((r1 + 1))
+        echo "${y1}.${w1}.${r1}"
+    fi
+}
+
+# compare_versions 2024.44.4 2024.44.0 # 2024.44.5
+# compare_versions 2024.44.4 2024.45.0 # 2024.45.0
+# compare_versions 2024.44.4 2025.1.0 # 2025.1.0
+
+CURRENT=$(grep version package.json | awk -F\" '{print $4}')
+NEXT=$(date +%Y.%W.0)
+VERSION=$(compare_versions "$CURRENT" "$NEXT")
+echo "Releasing $VERSION..."
+npm version "$VERSION" --include-workspace-root -ws
+sed "/version/ s/$CURRENT/$VERSION/" web/public/index.html > web/public/index.html.out ; mv web/public/index.html.out web/public/index.html
+git --no-pager diff
+git add package.json
+git commit --message "Release ${VERSION}"
--- web/src/pages/about.tsx
+++ web/src/pages/about.tsx
@@ -1,11 +1,23 @@
 import raw from "raw.macro";
 import Markdown from "../shell/markdown";
 
+const VERSION =
+  document.querySelector("meta[name=version]")?.getAttribute("content") ??
+  "unknown";
+
+const useVersion = () => {
+  return VERSION;
+};
+
 export const About = () => {
+  const version = useVersion();
   return (
     <div style={{ overflowY: "scroll" }}>
       <div className="container">
         <Markdown>{raw("./ABOUT.md")}</Markdown>
+        <p>
+          <b>Version</b> <code>{version}</code>
+        </p>
       </div>
     </div>
   );

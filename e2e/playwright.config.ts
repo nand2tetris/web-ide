3ab20c33f01334d040d0d60bc29aedf9793af2d6
David Souther <david.souther+github@gmail.com>
2026-04-21 10:46:35 -0400

    Add higher timeout for ci

--- e2e/playwright.config.ts
+++ e2e/playwright.config.ts
@@ -3,7 +3,7 @@ import { defineConfig, devices } from "@playwright/test";
 export default defineConfig({
   testDir: "tests",
   retries: 3,
-  timeout: 6_000,
+  timeout: 60_000,
   use: {
     baseURL: "http://localhost:3000/web-ide/",
     screenshot: "on",
